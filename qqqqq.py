9c3cf20cf2e87e7ff9890bdfa9608ade45ef669e
VoRBabek <153966817+VoRBabek@users.noreply.github.com>
2024-06-04 18:06:48 +0300

    Add files via upload

---
+++ qqqqq.py
@@ -0,0 +1,13 @@
+import pytest 
+
+def plus(a,b):
+    return a+b
+
+def minus(a,b):
+    return a-b
+
+def test_plus():
+    assert plus(1, 2) == 3 
+
+def test_minus():
+    assert minus (1,2) == -1
